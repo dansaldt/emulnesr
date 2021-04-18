60cbc257ec8f30d428d30257c5c2c9114f89a94b
dansaldt <dansaldt@protonmail.com>
2021-04-18 12:44:00 +0700

    Add memory addressing methods
    + add memory 0xffff (65536) bytes long
    + add read and write from/to memory
    + add load and run program, remove interpret method
    + unit tested all ok

--- src/cpu.rs
+++ src/cpu.rs
@@ -6,6 +6,7 @@ pub struct CPU {
     pub reg_x: u8,
     pub status: u8,
     pub pc: u16,
+    pub memory: [u8; 0xffff],
 }
 
 impl CPU {
@@ -15,7 +16,44 @@ impl CPU {
             reg_x: 0,
             status: 0,
             pc: 0,
+            memory: [0; 0xffff],
         }
+    }
+
+    pub fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    pub fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.run();
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.pc = 0x8000;
+    }
+
+    pub fn run(&mut self) {
+        // note: we move initialization of program counter from here to load function
+        loop {
+            let opcode = self.prog_read();
+
+            match opcode {
+                0xa9 | 0xa5 | 0xb5 | 0xad | 0xbd | 0xb9 | 0xa1 | 0xb1  => {
+                    let param = self.prog_read();
+                    self.op_lda(opcode, param);
+                }
+                0xaa => self.op_tax(),
+                0xe8 => self.op_inx(),
+                0x00 => return, // set break flag omitted as there's no affect
+                _ => todo!()
+            }
+        } // end loop
     }
 
     fn op_lda(&mut self, opcode: u8, value: u8) {
@@ -65,30 +103,12 @@ impl CPU {
         }
     }
 
-  fn read_byte (&mut self, program: &Vec<u8>) -> u8 {
-        let result = program[self.pc as usize];
+    fn prog_read (&mut self) -> u8 {
+        let result = self.mem_read(self.pc);
         self.pc += 1;
         result
     }
 
-    pub fn interpret(&mut self, program: Vec<u8>) {
-        self.pc = 0;
-
-        loop {
-            let opcode = self.read_byte(&program);
-
-            match opcode {
-                0xa9 | 0xa5 | 0xb5 | 0xad | 0xbd | 0xb9 | 0xa1 | 0xb1  => {
-                    let param = self.read_byte(&program);
-                    self.op_lda(opcode, param);
-                }
-                0xaa => self.op_tax(),
-                0xe8 => self.op_inx(),
-                0x00 => return, // set break flag omitted as there's no affect
-                _ => todo!()
-            }
-        } // end loop
-    }
 }
 
 
@@ -99,7 +119,7 @@ mod test {
     #[test]
     fn test_0xa9_lda_immediate_load_data() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
         assert_eq!(cpu.reg_a, 0x05);
         assert_eq!(cpu.reg_a & 0b0000_0010, 0b00);
         assert_eq!(cpu.reg_a & 0b1000_0000, 0);
@@ -108,7 +128,7 @@ mod test {
     #[test]
     fn test_0xa9_lda_zero_flag () {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
         assert_eq!(cpu.status & 0b0000_0010, 0b10);
     }
 
@@ -116,14 +136,14 @@ mod test {
     fn test_0xaa_tax_move_a_to_x() {
         let mut cpu = CPU::new();
         cpu.reg_a = 10;
-        cpu.interpret(vec![0xaa, 0x00]);
+        cpu.load_and_run(vec![0xaa, 0x00]);
         assert_eq!(cpu.reg_a, cpu.reg_x);
     }
 
     #[test]
     fn test_5_ops_working_together() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
 
         assert_eq!(cpu.reg_x, 0xc1)
     }
@@ -132,7 +152,7 @@ mod test {
     fn test_inx_overflow() {
         let mut cpu = CPU::new();
         cpu.reg_x = 0xff;
-        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+        cpu.load_and_run(vec![0xe8, 0xe8, 0x00]);
 
         assert_eq!(cpu.reg_x, 1)
     }
