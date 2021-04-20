531d9d28d8a2e3bb07abad19514312b08e153389
dansaldt <dansaldt@protonmail.com>
2021-04-21 03:10:45 +0700

    Add `get_operand_address` and refactor `op_lda`

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,4 @@
+use std::panic::panic_any;
 
 pub struct CPU {
     pub reg_a: u8,
@@ -81,10 +82,14 @@ impl CPU {
             let opcode = self.prog_read();
 
             match opcode {
-                0xa9 | 0xa5 | 0xb5 | 0xad | 0xbd | 0xb9 | 0xa1 | 0xb1  => {
-                    let param = self.prog_read();
-                    self.op_lda(opcode, param);
-                }
+                0xa9 => self.op_lda(&AddressingModes::Immediate),
+                0xa5 => self.op_lda(&AddressingModes::ZeroPage),
+                0xb5 => self.op_lda(&AddressingModes::ZeroPage_X),
+                0xad => self.op_lda(&AddressingModes::Absolute),
+                0xbd => self.op_lda(&AddressingModes::Absolute_X),
+                0xb9 => self.op_lda(&AddressingModes::Absolute_Y),
+                0xa1 => self.op_lda(&AddressingModes::Indirect_X),
+                0xb1 => self.op_lda(&AddressingModes::Indirect_Y),
                 0xaa => self.op_tax(),
                 0xe8 => self.op_inx(),
                 0x00 => return, // set break flag omitted as there's no affect
@@ -93,23 +98,69 @@ impl CPU {
         } // end loop
     }
 
-    fn op_lda(&mut self, opcode: u8, value: u8) {
-        // 0xa9 | 0xa5 | 0xb5 | 0xad | 0xbd | 0xb9 | 0xa1 | 0xb1
-        match opcode {
-            0xa9 => {
-                self.reg_a = value;
-                self.flag_update_zero(self.reg_a);
-                self.flag_update_negative(self.reg_a);
+    fn prog_read (&mut self) -> u8 {
+        let result = self.mem_read(self.pc);
+        self.pc += 1;
+        result
+    }
+
+    fn prog_read_u16(&mut self) -> u16 {
+        let result = self.mem_read_u16(self.pc);
+        self.pc += 2;
+        result
+    }
+
+    fn get_operand_address(&mut self, mode: &AddressingModes) -> u16 {
+        match mode {
+            AddressingModes::Immediate => {
+                self.pc
             },
-            0xa5 => todo!(),
-            0xb5 => todo!(),
-            0xad => todo!(),
-            0xbd => todo!(),
-            0xb9 => todo!(),
-            0xa1 => todo!(),
-            0xb1 => todo!(),
-            _ => panic!("op_lda invalid opcode {}", opcode)
+            AddressingModes::ZeroPage => {
+                self.prog_read() as u16
+            },
+            AddressingModes::ZeroPage_X => {
+                let pos = self.prog_read();
+                pos.wrapping_add(self.reg_x) as u16
+            },
+            AddressingModes::ZeroPage_Y => {
+                let pos = self.prog_read();
+                pos.wrapping_add(self.reg_y) as u16
+            },
+            AddressingModes::Absolute => {
+                self.prog_read_u16()
+            },
+            AddressingModes::Absolute_X => {
+                let base = self.prog_read_u16();
+                base.wrapping_add(self.reg_x as u16)
+            },
+            AddressingModes::Absolute_Y => {
+                let base = self.prog_read_u16();
+                base.wrapping_add(self.reg_y as u16)
+            },
+            AddressingModes::Indirect_X => {
+                let base = self.prog_read();
+                let ptr: u8 = (base as u8).wrapping_add(self.reg_x);
+                self.mem_read_u16(base as u16)
+            },
+            AddressingModes::Indirect_Y => {
+                let base = self.prog_read();
+                let addr = self.mem_read_u16(base as u16);
+                addr.wrapping_add(self.reg_y as u16)
+            },
+            AddressingModes::NoneAddressing => {
+                panic!("mode {:?} is not supported", mode)
+            },
         }
+    }
+
+    fn op_lda(&mut self, mode: &AddressingModes) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+        self.pc += 1;
+
+        self.reg_a = value;
+        self.flag_update_zero(self.reg_a);
+        self.flag_update_negative(self.reg_a);
     }
 
     fn op_tax(&mut self) {
@@ -138,12 +189,6 @@ impl CPU {
         } else {
             self.status &= 0b0111_1111;
         }
-    }
-
-    fn prog_read (&mut self) -> u8 {
-        let result = self.mem_read(self.pc);
-        self.pc += 1;
-        result
     }
 
 }
